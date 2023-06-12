37135f0cebf51ea0c4665238e712e943627e525d
Sergey Kovalev <dipdeepcode@mail.ru>
2023-06-12 16:01:08 +0300

    init commit

---
+++ src/main/java/ru/ddc/em/EmployeeManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.ddc.em;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagerApplication.class, args);
+	}
+
+}
