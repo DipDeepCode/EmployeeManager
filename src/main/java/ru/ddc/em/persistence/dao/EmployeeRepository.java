a236411a3a6868fd2e87462468654a49120a5b5d
Sergey Kovalev <dipdeepcode@mail.ru>
2023-06-12 23:01:24 +0300

    Добавлена аннотация Repository

--- src/main/java/ru/ddc/em/persistence/dao/EmployeeRepository.java
+++ src/main/java/ru/ddc/em/persistence/dao/EmployeeRepository.java
@@ -6,11 +6,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.ddc.em.persistence.model.Employee;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     @EntityGraph(attributePaths = "vacancy")
