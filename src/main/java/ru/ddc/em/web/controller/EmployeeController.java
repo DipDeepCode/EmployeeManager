c0165ab905232f91cb3517636104c41a3f8a8348
Sergey Kovalev <dipdeepcode@mail.ru>
2023-08-25 23:20:15 +0300

    Рефакторинг :: добавлены методы для перевода на другую вакансию и назначения на вакансию

--- src/main/java/ru/ddc/em/web/controller/EmployeeController.java
+++ src/main/java/ru/ddc/em/web/controller/EmployeeController.java
@@ -6,11 +6,14 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.ddc.em.persistence.entity.Employee;
+import ru.ddc.em.persistence.entity.Vacancy;
 import ru.ddc.em.service.EmployeeService;
+import ru.ddc.em.service.VacancyService;
 import ru.ddc.em.utils.custommapper.CustomMapper;
 import ru.ddc.em.web.dto.EmployeeDto;
 
 import jakarta.validation.Valid;
+import ru.ddc.em.web.dto.VacancyDto;
 
 import java.util.List;
 
@@ -18,10 +21,14 @@ import java.util.List;
 @RequestMapping("/employees")
 public class EmployeeController {
     private final EmployeeService employeeService;
+    private final VacancyService vacancyService;
     private final CustomMapper mapper;
 
-    public EmployeeController(EmployeeService employeeService, CustomMapper mapper) {
+    public EmployeeController(EmployeeService employeeService,
+                              VacancyService vacancyService,
+                              CustomMapper mapper) {
         this.employeeService = employeeService;
+        this.vacancyService = vacancyService;
         this.mapper = mapper;
     }
 
@@ -38,7 +45,10 @@ public class EmployeeController {
                        Model model) {
         Employee employee = employeeService.findById(personnelNumber);
         EmployeeDto employeeDto = mapper.map(employee, EmployeeDto.class);
-        model.addAttribute("employee", employeeDto);
+        model.addAttribute("employeeDto", employeeDto);
+        Vacancy vacancy = employee.getVacancy();
+        VacancyDto vacancyDto = vacancy != null ? mapper.map(vacancy, VacancyDto.class) : null;
+        model.addAttribute("vacancyDto", vacancyDto);
         return "employees/employee-show";
     }
 
@@ -48,6 +58,9 @@ public class EmployeeController {
         Employee employee = employeeService.findById(personnelNumber);
         EmployeeDto employeeDto = mapper.map(employee, EmployeeDto.class);
         model.addAttribute("employeeDto", employeeDto);
+        Vacancy vacancy = employee.getVacancy();
+        VacancyDto vacancyDto = vacancy != null ? mapper.map(vacancy, VacancyDto.class) : null;
+        model.addAttribute("vacancyDto", vacancyDto);
         return "employees/employee-edit";
     }
 
@@ -79,6 +92,59 @@ public class EmployeeController {
             employeeService.save(employee);
             return "redirect:/employees";
         }
+    }
+
+    @PatchMapping("/{personnelNumber}/removeFromVacancy")
+    public String removeFromVacancy(@PathVariable("personnelNumber") Long personnelNumber,
+                                    Model model) {
+        employeeService.removeFromVacancy(personnelNumber);
+        return "redirect:/employees/" + personnelNumber;
+    }
+
+    @GetMapping("/{personnelNumber}/transfer")
+    public String transferToAnotherVacancy(@PathVariable("personnelNumber") Long personnelNumber,
+                                           Model model) {
+        Employee employee = employeeService.findById(personnelNumber);
+        EmployeeDto employeeDto = mapper.map(employee, EmployeeDto.class);
+        model.addAttribute("employeeDto", employeeDto);
+
+        Vacancy vacancy = employee.getVacancy();
+        VacancyDto vacancyDto = vacancy != null ? mapper.map(vacancy, VacancyDto.class) : null;
+        model.addAttribute("vacancyDto", vacancyDto);
+
+        List<Vacancy> vacancyList = vacancyService.findByEmployeeNull();
+        List<VacancyDto> vacancyDtoList = mapper.mapIterable(vacancyList, VacancyDto.class);
+        model.addAttribute("vacancyDtoList", vacancyDtoList);
+        return "/employees/employee-transfer";
+    }
+
+    @PatchMapping("/{personnelNumber}/transfer")
+    public String transferToAnotherVacancy(@PathVariable("personnelNumber") Long personnelNumber,
+                                           @RequestParam("vacancyId") Long vacancyId,
+                                           Model model) {
+        employeeService.transferToAnotherVacancy(personnelNumber, vacancyId);
+        return "redirect:/employees/" + personnelNumber;
+    }
+
+    @GetMapping("/{personnelNumber}/appoint")
+    public String appointToVacancy(@PathVariable("personnelNumber") Long personnelNumber,
+                                   Model model) {
+        Employee employee = employeeService.findById(personnelNumber);
+        EmployeeDto employeeDto = mapper.map(employee, EmployeeDto.class);
+        model.addAttribute("employeeDto", employeeDto);
+
+        List<Vacancy> vacancyList = vacancyService.findByEmployeeNull();
+        List<VacancyDto> vacancyDtoList = mapper.mapIterable(vacancyList, VacancyDto.class);
+        model.addAttribute("vacancyDtoList", vacancyDtoList);
+        return "/employees/employee-appoint";
+    }
+
+    @PatchMapping("/{personnelNumber}/appoint")
+    public String appointToVacancy(@PathVariable("personnelNumber") Long personnelNumber,
+                                   @RequestParam("vacancyId") Long vacancyId,
+                                   Model model) {
+        employeeService.appointToVacancy(personnelNumber, vacancyId);
+        return "redirect:/employees/" + personnelNumber;
     }
 
     @DeleteMapping("/{personnelNumber}")
